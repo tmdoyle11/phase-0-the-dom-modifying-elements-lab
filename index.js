f99cf73c62b3c754ae3f1747e28a1ac50889b03c
Thomas Doyle <tmdoyle11@gmail.com>
2022-02-28 16:19:56 -0600

    commit tree d3d1c6ca03d45d6f776d1825c03349a6c69b9df9

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const mainElement = document.querySelector("#main").remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "Thomas is the champion"
